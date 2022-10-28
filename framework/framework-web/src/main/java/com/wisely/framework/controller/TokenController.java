89a59a1a26268e3932b2c6a013dfd5f4d951b01e
chengyi <chengyi@sec-dt.com>
2022-10-28 08:49:08 +0800

    代码优化
    (cherry picked from commit 535892a15e2ba228aabf7e3253a3e7b8b3ac12be)

--- framework/framework-web/src/main/java/com/wisely/framework/controller/TokenController.java
+++ framework/framework-web/src/main/java/com/wisely/framework/controller/TokenController.java
@@ -4,8 +4,8 @@ import com.wisely.framework.exception.SystemException;
 import com.wisely.framework.helper.RandomHelper;
 import com.wisely.framework.helper.RequestHelper;
 import com.wisely.framework.helper.ResponseBuilder;
-import com.wisely.framework.helper.lock.DoUnionLock;
-import com.wisely.framework.helper.lock.DoUnionLockFactory;
+import com.wisely.framework.helper.lock.WiselyLock;
+import com.wisely.framework.helper.lock.WiselyLockFactory;
 import com.wisely.framework.plugins.token.TokenProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/token")
 public class TokenController {
 
-    public TokenController(DoUnionLockFactory doUnionLockFactory, TokenProperties tokenProperties) {
+    public TokenController(WiselyLockFactory wiselyLockFactory, TokenProperties tokenProperties) {
         this.expiredTime = tokenProperties.getExpiredTime();
-        this.doUnionLock = doUnionLockFactory.build(tokenProperties.getModel(), this.expiredTime);
+        this.wiselyLock = wiselyLockFactory.build(tokenProperties.getModel(), this.expiredTime);
     }
 
 
-    DoUnionLock doUnionLock;
+    WiselyLock wiselyLock;
 
     int expiredTime;
 
@@ -30,8 +30,8 @@ public class TokenController {
     public Object get() {
 
         String token = RandomHelper.uuid();
-        doUnionLock.unlock(token); // 复用了 DoUnionLock, 感觉不是很好...
-        boolean flag = doUnionLock.lock(RequestHelper.getSessionId(), token, this.expiredTime * 1000); // 默认5分钟
+        wiselyLock.unlock(token); // 复用了 DoUnionLock, 感觉不是很好...
+        boolean flag = wiselyLock.lock(RequestHelper.getSessionId(), token, this.expiredTime * 1000); // 默认5分钟
         if (!flag) {
             throw SystemException.builder("token build failed");
         }
